bcc220023fa131508431fabbcd7f6252d056e04c
ASHUTOSH GOYAL <ashutoshgoyal46@gmail.com>
2024-09-06 08:13:33 +0000

    add with clause for better code readibility

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,8 @@
 
 -- Use the `ref` function to select from other models
 
-select *
+with second as 
+(select *
 from {{ ref('my_first_dbt_model') }}
-where id = 1
+where id = 1)
+select * from second
